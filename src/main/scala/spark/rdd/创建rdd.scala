f3a8da5ca1191cd64f130eae31a387e17a5589b9
hello <pzghost123@gmail.com>
2024-02-19 12:18:31 +0800

    RDD的并行度 & 分区

--- src/main/scala/spark/rdd/创建rdd.scala
+++ src/main/scala/spark/rdd/创建rdd.scala
@@ -24,6 +24,28 @@ object 创建rdd {
     val fileRDD1: RDD[(String, String)] = sc.wholeTextFiles("F:\\Java-learn\\hadoop\\资料\\11_input\\inputword\\hello.txt")
     fileRDD1.collect().foreach(println)
 
+    //TODO RDD的并行度
+    // RDD的并行度 & 分区
+    // 1)makeRDD方法可以传递第二个参数，这个参数表示分区的数量
+    // 第二个参数可以不传递的，那么makeRDD方法会使用默认值 ： defaultParallelism（默认并行度）
+    //     scheduler.conf.getInt("spark.default.parallelism", totalCores)
+    //    spark在默认情况下，从配置对象中获取配置参数：spark.default.parallelism
+    //    如果获取不到，那么使用totalCores属性，这个属性取值为当前运行环境的最大可用核数
+    //val rdd1 = sc.makeRDD(List(1,2,3,4),2)
+    val rdd1 = sc.makeRDD(List(1,2,3,4))
+    // 2)textFile可以将文件作为数据处理的数据源，默认也可以设定分区。
+    //     minPartitions : 最小分区数量
+    //     math.min(defaultParallelism, 2)
+    //val rdd = sc.textFile("datas/1.txt")
+    // 如果不想使用默认的分区数量，可以通过第二个参数指定分区数
+    // Spark读取文件，底层其实使用的就是Hadoop的读取方式
+    // 分区数量的计算方式：
+    //    totalSize = 7  比如有三个数据1,2,3是3个字节，但是还要加上回车符则为3+4=7个字节
+    //    goalSize =  7 / 2 = 3（byte）  每个分区应存放3个字节
+    //    7 / 3 = 2...1 (1.1) + 1 = 3(分区)   一共是3个分区
+
+    val rdd2 = sc.textFile("datas/1.txt", 2)
+
     //TODO 关闭环境
     sc.stop()
   }
